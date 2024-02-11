e4d29d821561df13c2616f27ee071724a5ed5633
Stalkermtx <106044847+Stalkermtx@users.noreply.github.com>
2024-02-11 12:48:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@
 echo
 echo "Atualizando bot, aguarde..."
 echo
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/index.js > /dev/null 2>&1
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/veri.js > /dev/null 2>&1
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/gerar.js > /dev/null 2>&1
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/index.js > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/veri.js > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/gerar.js > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/qrcode > /dev/null 2>&1
 chmod +x qrcode
 mv qrcode /bin
 mv index.js /etc/megahbot
@@ -31,13 +31,13 @@ apt install nodejs -y > /dev/null 2>&1
 apt install unzip -y > /dev/null 2>&1
 apt install screen -y > /dev/null 2>&1
 apt install wget -y > /dev/null 2>&1
-wget https://raw.githubusercontent.com/penguinehis/WaBot-VendasSSH/main/mult/onbot > /dev/null 2>&1
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/offbot > /dev/null 2>&1
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/config.js > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Stalkermtx/WaBot-VendasSSH/main/mult/onbot > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/offbot > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/qrcode > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/config.js > /dev/null 2>&1
 chmod +x onbot offbot qrcode
 mv onbot offbot qrcode /bin
-wget https://github.com/penguinehis/WaBot-VendasSSH/raw/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/blob/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1
 unzip /etc/megahbot.zip
 mv megahbot /etc
 echo
