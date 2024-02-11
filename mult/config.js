455c17bafb87e697a2f9d22706ca5076f1f53e8a
Stalkermtx <106044847+Stalkermtx@users.noreply.github.com>
2024-02-11 12:39:43 -0400

    Update config.js

--- mult/config.js
+++ mult/config.js
@@ -1,9 +1,9 @@
 config = {
-  dono:"559184035474", //seu número com 55+ddd sem o 9
+  dono:"5565984739398", //seu número com 55+ddd sem o 9
   tempo_teste:"3", //tempo do teste em horas
   valorLogin: 0.10, //valor do login SSH
-  nomeLoja:"Loja teste", //nome da sua loja virtual
-  linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
+  nomeLoja:"Start Net", //nome da sua loja virtual
+  linkApp:"https://download1584.mediafire.com/5yessp2sfbpghIIJG_aKYTAAUjW5AsROnsO8enzWZXtUBVzRw9BGHB6cJ_B8b0gi8itUzLLWaL6AWsG-nsA_xVWe54HvCd6d26oXbgn-ULiIqe_ec0wkECsPYUBtSW_xQWe_Ay1WbnCmSj9hrtgiQ4MtsJDS9B5OvlAw3m51p_1lUg/mpu6ydbm06tp000/base.apk", //Link do seu app, playStore/Mediafire
   token_mp:"TokenAqui" //seu token do mercado pago
 }
 module.exports = {
